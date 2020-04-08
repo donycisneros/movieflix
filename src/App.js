7b736e0489eaec2ad6692fa370ade744660fcf7c
Dony Cisneros <dony.cisne@gmail.com>
2020-04-08 01:15:36 -0300

    Initial setup movieflix

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => {
+  return (
+    <main>
+      <h1>Movieflix</h1>
+    </main>
+  );
+};
+
+render(<App />, document.getElementById("root"));
