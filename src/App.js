3779dc5ad209b8bc232c6042b0ee6a34ecc7f806
Dony Cisneros <dony.cisne@gmail.com>
2020-04-11 23:23:58 -0300

    Navigation - Integration (wip: Scroll)

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React from "react";
 import Header from "./components/Header/Header";
 
 const App = () => {
+  let loop = [];
+  for (let i = 0; i < 80; i++) {
+    loop.push(i);
+  }
+
   return (
     <div className="movieflix">
       <Header />
+      {loop.map((l) => (
+        <p key={l}>{l}</p>
+      ))}
     </div>
   );
 };
