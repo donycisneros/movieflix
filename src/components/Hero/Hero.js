2d09292803a60d1e6c089e2ac788f9fcd38cacb4
Dony Cisneros <dony.cisne@gmail.com>
2020-04-19 23:36:54 -0300

    Hero - Partial Fetch Integration

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Hero = () => {
+  const [heroPoster, setHeroPoster] = useState("");
+
+  useEffect(() => {
+    const fetchPoster = async () => {
+      const poster = await axios(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_TMDB_API_KEY}`
+      );
+      setHeroPoster(poster);
+    };
+    fetchPoster();
+  }, []);
+
   return (
     <div className="hero">
-      <h1>Hello</h1>
+      <img src={heroPoster} alt="Poster" />
     </div>
   );
 };
